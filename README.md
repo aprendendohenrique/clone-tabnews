d69dafbaa11eebb446e7504e0bc36d11ab8b85f4
aprendendohenrique <henriquericardofodi@gmail.com>
2026-01-06 18:21:51 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para treinar programação. Sendo uma implementação do https://tabnews.com.br por causa do curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-    return <h1>Guilherme, olha que massa kkkkkkk! Sim eu fiz um site! E ta tudo funcionando! Kkkkkkk</h1>
+function Home() {
+  return (
+    <h1>
+      Guilherme, olha que massa kkkkkkk! Sim eu fiz um site! E ta tudo
+      funcionando! Kkkkkkk
+    </h1>
+  );
 }
 
 export default Home;
